7975a50852f4ab08c36669886a8f6c3488dd4837
= <=>
2021-06-14 08:26:27 +0545

    June 14, 2021 Updated

--- Core Java/FirstProject/src/pkg1/First.java
+++ Core Java/FirstProject/src/pkg1/First.java
@@ -2,7 +2,7 @@ package pkg1;
 
 public class First {
 	public static void main(String[] args) {
-		System.out.println("Hello world of Java-1");
+		System.out.println("Hello world of Java");
 		System.out.println("Hi");
 		System.out.println("How are you?");
 	}
