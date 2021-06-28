873dd4dd664e6dcec445826001d72c23091ac4c4
Krishna Aryal <info.karyal@gmail.com>
2021-06-29 05:39:35 +0545

    June 29, 2021-2

--- Core Java/FirstProject/src/unit1_task/MyCalculator.java
+++ Core Java/FirstProject/src/unit1_task/MyCalculator.java
@@ -2,9 +2,38 @@ package unit1_task;
 
 public class MyCalculator {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {		
+		//Declare
+		byte choice=-1;				
+		int num1, num2, num3;
 		
-
+		MyFunctions.printMenu();//Print Main Menu
+		choice = MyFunctions.readChoice();//Accept Menu Option
+		
+		if(choice == 1) {
+			//Sum
+			num1 = MyFunctions.readNumber("Enter first no : ");
+			num2 = MyFunctions.readNumber("Enter first no : ");
+			num3 = MyFunctions.calcSum(num1, num2);
+			MyFunctions.printResult("SUM", num3);
+		}
+		else if(choice == 2) {
+			//Diff
+			num1 = MyFunctions.readNumber("Enter first no : ");
+			num2 = MyFunctions.readNumber("Enter first no : ");
+			num3 = MyFunctions.calcSub(num1, num2);
+			MyFunctions.printResult("DIFF", num3);
+		}
+		else if(choice == 3) {
+			//Prd
+			num1 = MyFunctions.readNumber("Enter first no : ");
+			num2 = MyFunctions.readNumber("Enter first no : ");
+			num3 = MyFunctions.calcPrd(num1, num2);
+			MyFunctions.printResult("PRD", num3);
+		}
+		else if(choice == 0) {
+			//Exit
+			MyFunctions.exit();			
+		}
 	}
-
 }
--- Core Java/FirstProject/src/unit1_task/MyFunctions.java
+++ Core Java/FirstProject/src/unit1_task/MyFunctions.java
@@ -1,8 +1,10 @@
 package unit1_task;
 
+import java.util.Scanner;
+
 public class MyFunctions {
 	
-	void printMenu() {
+	static void printMenu() {
 		System.out.println("----------------------------------"); 
 		System.out.println("Main Menu"); 
 		System.out.println("----------------------------------");
@@ -11,5 +13,37 @@ public class MyFunctions {
 		System.out.println("3. PRD");
 		System.out.println("0. EXIT");
 		System.out.println("----------------------------------");
+	}
+	
+	static byte readChoice() {
+		byte tmpChoice;		
+		System.out.print("Enter your choice : _ ");
+		tmpChoice = Byte.parseByte(new Scanner(System.in).nextLine());
+		System.out.println("----------------------------------");
+		return tmpChoice;
+	}	
+	
+	static int readNumber(String strMessage) {
+		int tmpChoice;		
+		System.out.print(strMessage);
+		tmpChoice = Integer.parseInt(new Scanner(System.in).nextLine());
+		return tmpChoice; 
+	}
+	
+	static int calcSum(int num1, int num2) {
+		return (num1+num2);
+	}
+	static int calcSub(int num1, int num2) {
+		return (num1-num2);
+	}
+	static int calcPrd(int num1, int num2) {
+		return (num1*num2);
+	}
+	static void exit() {
+		System.out.println("Bye !");
+		System.exit(0);
+	}
+	static void printResult(String strLabel, int result) {
+		System.out.println(strLabel+" "+result);
 	}
 }
